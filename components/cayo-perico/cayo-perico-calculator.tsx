dd1250d0305239a188375c3fcdca9b68993dbfef
12LuA <71656192+12LuA@users.noreply.github.com>
2026-03-22 13:14:22 +0100

    feat: simplify Card component in financial breakdown section

--- components/cayo-perico/cayo-perico-calculator.tsx
+++ components/cayo-perico/cayo-perico-calculator.tsx
@@ -534,7 +534,7 @@ export default function CayoPericoCalculator() {
             </Card>
 
             <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-              <Card className="border-t-4 border-t-primary">
+              <Card>
                 <CardHeader>
                   <CardTitle>Financial Breakdown</CardTitle>
                 </CardHeader>
